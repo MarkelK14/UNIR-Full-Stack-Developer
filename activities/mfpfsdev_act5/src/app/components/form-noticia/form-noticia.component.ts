c0c07e016d606b40454c5630e0c4a9b471cccf56
Markel Cortajarena <markelkortajarena@gmail.com>
2025-02-14 11:33:40 +0100

    componentes creados

---
+++ activities/mfpfsdev_act5/src/app/components/form-noticia/form-noticia.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-noticia',
+  imports: [],
+  templateUrl: './form-noticia.component.html',
+  styleUrl: './form-noticia.component.css'
+})
+export class FormNoticiaComponent {
+
+}
