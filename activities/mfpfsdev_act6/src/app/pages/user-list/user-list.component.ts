3120ac91ae738e037b873ea85c3dab92256f995b
Markel Cortajarena <markelkortajarena@gmail.com>
2025-03-11 22:22:26 +0100

    lectura de datos de la API

--- activities/mfpfsdev_act6/src/app/pages/user-list/user-list.component.ts
+++ activities/mfpfsdev_act6/src/app/pages/user-list/user-list.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { IUsuario } from '../../interfaces/iusuario.interface';
+import { UsuariosService } from '../../services/usuarios.service';
+import { IResponse } from '../../interfaces/iresponse.interface';
 
 @Component({
   selector: 'app-user-list',
@@ -7,5 +10,20 @@ import { Component } from '@angular/core';
   styleUrl: './user-list.component.css'
 })
 export class UserListComponent {
+  arrUsuarios: IUsuario[] = [];
+  usuariosServices = inject(UsuariosService);
 
+  async ngOnInit() {
+    this.cargarPersonajes();
+  }
+
+  async cargarPersonajes() {
+    try {
+      let response: IResponse = await this.usuariosServices.getAllPromise();
+      this.arrUsuarios = response.results;
+      console.log(`Page: ${response.page}, Per Page: ${response.per_page}, Total: ${response.total}, Total Pages: ${response.total_pages}`);
+    } catch (error) {
+      console.log(error);
+    }
+  }
 }
---
+++ activities/mfpfsdev_act6/src/app/services/usuarios.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { lastValueFrom } from 'rxjs';
+import { IResponse } from '../interfaces/iresponse.interface';
+import { IUsuario } from '../interfaces/iusuario.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsuariosService {
+  // para que un servicio pueda hacer una petición tiene que inyectar el httpClient
+  private httpClient = inject(HttpClient)
+  private baseUrl: string = "https://peticiones.online/api/users";
+
+  getAllPromise(): Promise<IResponse> {
+    console.log
+    return lastValueFrom(this.httpClient.get<IResponse>(this.baseUrl))
+  }
+}
