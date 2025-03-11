df14b92875963e89c4ce92b892a79cc09605a55b
Markel Cortajarena <markelkortajarena@gmail.com>
2025-03-11 16:04:17 +0100

    userListComponent creado. Empezando a organizar rutas. Header creado

--- activities/mfpfsdev_act6/src/app/app.routes.ts
+++ activities/mfpfsdev_act6/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { UserListComponent } from './pages/user-list/user-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: "", pathMatch: 'full', redirectTo: 'home' },
+    { path: "home", component: UserListComponent },
+    { path: '**', redirectTo: 'home' }
+];
