9521fd030ace52c06a42f5a1413401c2ecb27ec9
Markel Cortajarena <markelkortajarena@gmail.com>
2025-03-16 13:15:23 +0100

    userForm recoge datos

--- activities/mfpfsdev_act6/src/app/pages/usuarios-form/usuarios-form.component.ts
+++ activities/mfpfsdev_act6/src/app/pages/usuarios-form/usuarios-form.component.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { IUsuario } from '../../interfaces/iusuario.interface';
 
 @Component({
   selector: 'app-usuarios-form',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './usuarios-form.component.html',
   styleUrl: './usuarios-form.component.css'
 })
 export class UsuariosFormComponent {
+  @Input() idUser: string = ""
+  userForm: FormGroup = new FormGroup({}, [])
+  selectedUser!: IUsuario;
 
+  ngOnInit(){
+    this.userForm = new FormGroup({
+      _id: new FormControl(this.idUser || null, []),
+      first_name: new FormControl(this.selectedUser?.first_name || "", []),
+      last_name: new FormControl(this.selectedUser?.last_name || "", []),
+      username: new FormControl(this.selectedUser?.username || "", []),
+      email: new FormControl(this.selectedUser?.email || "", []),
+      password: new FormControl(this.selectedUser?.password || "", []),
+    }, [])
+  }
+
+  getDataForm(){
+    console.log(this.userForm.value)
+  }
 }
