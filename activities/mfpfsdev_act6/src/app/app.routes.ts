1c5c567152572d9be070e8e317d753db8eaf6d71
Markel Cortajarena <markelkortajarena@gmail.com>
2025-03-16 12:58:13 +0100

    componente UsuariosForm creado. app.routes.ts modificado incluyendo el componente nuevo. Header con la ruta a usuariosForm

--- activities/mfpfsdev_act6/src/app/app.routes.ts
+++ activities/mfpfsdev_act6/src/app/app.routes.ts
@@ -2,10 +2,13 @@ import { Routes } from '@angular/router';
 import { UserListComponent } from './pages/user-list/user-list.component';
 import { VistaUsuarioComponent } from './pages/vista-usuario/vista-usuario.component';
 import { Error404Component } from './pages/error404/error404.component';
+import { UsuariosFormComponent } from './pages/usuarios-form/usuarios-form.component';
 
 export const routes: Routes = [
     { path: "", pathMatch: 'full', redirectTo: 'home' },
     { path: "home", component: UserListComponent },
+    { path: 'usuario/new', component: UsuariosFormComponent },
     { path: "usuario/:idUsuario", component: VistaUsuarioComponent },
+    { path: "usuario/update/:idUsuario", component: UsuariosFormComponent },
     { path: '**', component: Error404Component }
 ];
---
+++ activities/mfpfsdev_act6/src/app/pages/usuarios-form/usuarios-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-usuarios-form',
+  imports: [],
+  templateUrl: './usuarios-form.component.html',
+  styleUrl: './usuarios-form.component.css'
+})
+export class UsuariosFormComponent {
+
+}
