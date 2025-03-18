1902422f1b02ad853db877d92732d43fdf491e48
Markel Cortajarena <markelkortajarena@gmail.com>
2025-03-18 16:55:56 +0100

    mejora en los estilos de las cards

--- activities/mfpfsdev_act6/src/app/components/usuario-card/usuario-card.component.ts
+++ activities/mfpfsdev_act6/src/app/components/usuario-card/usuario-card.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { IUsuario } from '../../interfaces/iusuario.interface';
 import { ButtonsComponent } from '../../shared/buttons/buttons.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-usuario-card',
-  imports: [ButtonsComponent],
+  imports: [ButtonsComponent, RouterLink],
   templateUrl: './usuario-card.component.html',
   styleUrl: './usuario-card.component.css'
 })
